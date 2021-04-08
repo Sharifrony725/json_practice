8bf7543e3f61186788b7ddd90583cb3fb8b42299
Rony <sharifrony725@gmail.com>
2021-04-09 00:00:42 +0600

    new file added

---
+++ index.js
@@ -0,0 +1,20 @@
+//const data = require("./friends_data.json");
+//console.log(data);
+//console.log(data.friends[0]);
+//console.log(data.friends[0].name);
+// data.friends[0] .name = "Shariful Islam"; 
+// console.log(data.friends[0].name);
+// delete data.friends[0].name;
+// console.log(data);
+// const data = require("./students_data.json");
+// // console.log(data.students[1].language[1]);
+// for( x in data ){
+//     console.log(x);
+//     console.log(data[x]);
+// }
+// const data2 = {
+//     name : "Rony",
+//     age : 23,
+// };
+// console.log(JSON.stringify(data2));
+console.log(JSON.parse('{"name" : "Rony"}'));
